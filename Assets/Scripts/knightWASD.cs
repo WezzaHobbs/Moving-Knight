c50985d50df4938c840b8b7dec97e86c08e34cc5
WezzaHobbs <WezzaHobbs@users.noreply.github.com>
2020-10-05 12:16:25 +0100

    Initial commit

---
+++ Assets/Scripts/knightWASD.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knightWASD : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            float ySpeed = 16.0f;
+            rb.velocity = new Vector3(ySpeed * 0, Time.deltaTime, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            float xSpeed = 5.0f;
+            transform.position -= new Vector3(xSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            float xSpeed = 5.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            float ySpeed = 8.0f;
+            transform.position -= new Vector3(ySpeed * 0, Time.deltaTime, 0);
+        }
+    }
+}
