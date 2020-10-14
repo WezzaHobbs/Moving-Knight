f09673ad874371363a27387fbb6aac96491367c1
WezzaHobbs <WezzaHobbs@users.noreply.github.com>
2020-10-14 16:32:33 +0100

    14/10 Work

--- Assets/Scripts/knightWASD.cs
+++ Assets/Scripts/knightWASD.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class knightWASD : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    public Sprite firstSprite;
+    public Sprite secondSprite;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         int xSpeed = 5;
-        int ySpeed = 5;
+        int ySpeed = 6;
         bool isGrounded = true;
 
         Vector3 velocity = rb.velocity;
@@ -29,6 +30,8 @@ public class knightWASD : MonoBehaviour
         if (Input.GetKey("s"))
         {
             velocity.y = -ySpeed;
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = secondSprite;
         }
         rb.velocity = velocity;
         if (Input.GetKey("a"))
@@ -42,6 +45,16 @@ public class knightWASD : MonoBehaviour
             velocity.x = xSpeed;
         }
         rb.velocity = velocity;
+        if (velocity.y > 0)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = firstSprite;
+        }
+        if (velocity.y <= 0)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = secondSprite;
+        }
     }
     void OnTriggerEnter2D(Collider2D col)
     {
